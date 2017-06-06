f8465176d88554fe7769ee3c14868775ca33e8ed
shenruipanda <panshenrui@sina.com>
2017-06-05 20:09:42 -0500

    Working perfectly!!!
    With a precision of 100%. I am so smart. ^_^Prof Burbank helped a lot in figuring out why my weights could go ridiculously large.

--- sonar_withoutcv.py
+++ sonar_withoutcv.py
@@ -1,8 +1,6 @@
 from csv import reader
 from numpy import *
 import numpy as np
-#np.seterr(all='ignore')
-import random as rd
 
 ##### Preparing dataset #####
 
@@ -47,33 +45,39 @@ str_column_to_int(dataset, len(dataset[0])-1)
 # convert dataset from list to array
 dataset = asarray(dataset)
 
+### Algorithm ###
+
 l_rate = 0.01        # Learning rate
 n_epoch = 1000  		  # Number of iterations
 l_hidden_layer = 61  # Number of neurons in hidden layer
 
 # Training weights
-np.random.seed(2)
-weights1 = 2*np.random.random((l_hidden_layer, len(dataset[0, :])-1)) -1  # Mean zero
+np.random.seed(222)
+# Mean zero; The reason why I devide the whole term by 100 is that the weights were too big for this dataset.
+weights1 = (2*np.random.random((l_hidden_layer, len(dataset[0, :])-1)) -1)/100
 #bias1 = 2*np.random.random((l_hidden_layer, len(dataset[:,0])))-1
 bias1 = np.zeros((1, len(dataset[:,0])))
-weights2 = 2*np.random.random((1, l_hidden_layer))-1
+weights2 = (2*np.random.random((1, l_hidden_layer))-1)/100
 #bias2 = 2*np.random.random((1, len(dataset[:,0])))-1
 bias2 = np.zeros((1, len(dataset[:,0])))
 
 for epoch in range(n_epoch):
-	hidden = np.dot(weights1, (dataset[:, 0:60]).T) + np.repeat(bias1, l_hidden_layer, axis = 0)  # hidden represents the values in hidden layer.
+	hidden = np.dot(weights1, (dataset[:, 0:60]).T) + np.repeat(bias1, l_hidden_layer, axis = 0)
+	# hidden represents the values in hidden layer.
+	hidden = 1 / (1 + np.exp(-hidden))
 	prediction = np.dot(weights2, hidden) + bias2
-	#prediction = np.exp(prediction) / (1 + np.exp(prediction))  # I can't use sigmoid function in here due to overflow problem in exp
-	for i in range(len(prediction[0, :])):
-		if prediction[0,i] >= 0.0:
-			prediction[0,i] = 1.0
-		else:
-			prediction[0,i] = 0.0
+	#prediction = 1 / (1 + np.exp(-prediction))
+	#  I am going to use step function here. In this way I can get the right value.
+	if True:
+		for i in range(len(prediction[0, :])):
+			prediction[0, i] = 1.0 if prediction[0,i] >= 0.0 else 0.0
 	error2 = dataset[:, -1] - prediction
 	error1 = np.dot(weights2.T, error2)
 	weights2 += l_rate * np.dot(error2, hidden.T)
+	#weights2 *= 0.8
 	bias2 += l_rate * error2
 	weights1 += l_rate * np.dot(error1, dataset[:, 0:60])
+	#weights1 *= 0.8
 	bias1 += l_rate * error1[0, ]
 print prediction
 
