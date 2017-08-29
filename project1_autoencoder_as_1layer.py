e7422bac63fab9c646b5b0b23e1bad3094b25e81
shenruipanda <panshenrui@sina.com>
2017-08-28 20:58:45 -0500

    regular AE + fc

--- project1_autoencoder_as_1layer.py
+++ project1_autoencoder_as_1layer.py
@@ -2,28 +2,31 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 from tensorflow.examples.tutorials.mnist import input_data
-from pylab import *
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+import matplotlib.pyplot as plt
+from pylab import *
+
 import tensorflow as tf
 import numpy as np
+tf.set_random_seed(123)
+np.random.seed(123)
 
 x = tf.placeholder(tf.float32, [None, 784])  # Input layer
 y_ = tf.placeholder(tf.float32, [None, 10])  # True values for output layer
-W1e = tf.Variable(tf.truncated_normal([784, 300], stddev = 0.1, seed = 123)) # encoder step
-b1e = tf.Variable(tf.zeros([300]))
-W1d = tf.Variable(tf.truncated_normal([300, 784], stddev = 0.1, seed = 123)) # decoder step
+W1e = tf.Variable(tf.truncated_normal([784, 100], stddev = 0.1, seed = 123)) # encoder step
+b1e = tf.Variable(tf.zeros([100]))
 b1d = tf.Variable(tf.zeros([784]))
-W2 = tf.Variable(tf.truncated_normal([300, 10], stddev=0.1, seed = 123))
+W2 = tf.Variable(tf.truncated_normal([100, 10], stddev=0.1, seed = 123)) # fully connected layer
 b2 = tf.Variable(tf.zeros([10]))
 
 h = tf.nn.sigmoid(tf.matmul(x, W1e) + b1e) # hidden layer
 y = tf.matmul(h, W2) + b2
 
-loss1 = tf.reduce_sum(tf.square(tf.nn.sigmoid(tf.matmul(h, W1d)+b1d) - x))
+loss1 = tf.reduce_sum(tf.square(tf.nn.sigmoid(tf.matmul(h, tf.transpose(W1e))+b1d) - x))
 loss2 = tf.reduce_mean(
       tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
-train_step1 = tf.train.GradientDescentOptimizer(0.05).minimize(loss1, var_list=[W1e, b1e, W1d, b1d])
+train_step1 = tf.train.GradientDescentOptimizer(0.5).minimize(loss1, var_list=[W1e, b1e, b1d])
 train_step2 = tf.train.GradientDescentOptimizer(0.5).minimize(loss2, var_list=[W2, b2])
 
 sess = tf.InteractiveSession()
@@ -44,8 +47,12 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                     y_: mnist.test.labels}))
 
-# visualize filters
-pixels = W1e[:,208].eval() # convert tensor to array
-pixels = pixels.reshape((28, 28))
-imshow(pixels, cmap = 'winter')
-show()
+#visualize filters
+for i in range(100):
+    pixels = W1e[:,i].eval() # convert tensor to array
+    pixels = pixels.reshape((28, 28))
+    plt.subplot(10, 10, i+1)
+    plt.imshow(pixels, cmap = 'winter')
+    plt.axis('off')
+    
+
